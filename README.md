75ce20ab6ac18e7a3b81ed91a0fb05c7c5a338ca
danhhuynh25029 <danhhuynh250@gmail.com>
2022-07-01 13:58:05 +0700

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # understand-channel-golang
 Repository helpful for beginner Golang
+
+Reference : [Soham Kamani][1]
+
+[1]:https://www.sohamkamani.com/golang/channels/
---
+++ channel.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main(){
+	// khoi tao channel
+	value := make(chan int,3)
+	
+	defer close(value)
+	//  tao ra 2 go routine
+	go addTwo(1,value)
+	go addTwo(2,value)
+	
+	// result := <- value
+	// lay ra gia tri value
+	fmt.Println(<-value)
+}
+
+func addTwo(number int,value chan int){
+	// gan gia tri vao channel
+	value <- number
+}
